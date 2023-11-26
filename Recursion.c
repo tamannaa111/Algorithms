84ae454101914d1f4136f8ad4e78527c38e3fc61
Tamanna Akter <144879732+tamannaa111@users.noreply.github.com>
2023-11-26 05:31:31 -0800

    Add files via upload

---
+++ Recursion.c
@@ -0,0 +1,27 @@
+//
+//  main.c
+//  recursion
+//
+//  Created by Tamanna Akter on 2/8/23.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    for(int i=1;i<=100;i=i+1)
+    {  if(i==5){
+        
+        continue;
+        
+    }
+      
+         
+        printf("%d\n",i);
+}
+  
+    return 0;
+}
+
+
+    
